bdde6b652f1f1980787ce1d2c6be3f0a623d4b2d
Pranay Mandadapu <pranaymandadapu@Pranays-Laptop.local>
2023-11-03 15:44:18 -0500

    some comments added

--- src/App.js
+++ src/App.js
@@ -1,60 +1,72 @@
+// Import necessary React, Router and component modules
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Component imports
 import Login from "./Login";
 import Register from "./Register";
 import Confirmation from "./Confirmation";
 import Home from "./Home";
 import Poster from "./Poster";
+// Global CSS import
 import "./index.css";
 import NotFound from "./404";
 import EditRound from "./EditRound";
 import Round from "./Round";
 import Refresh from './Refresh';
 
-
-// Bootstrap CSS
+// Bootstrap CSS and JS for styling and interactive components
 import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+// Navigation bar component
 import Navbar from "./Navbar";
 import "./index.css";
 
+// PrivateRoute component for protected routes
 import PrivateRoute from "./PrivateRoute";
 
-
+// Main App component which holds the structure of the entire application
 function App() {
-
+  // Helper function to concatenate the API URL with a path
   function getApiUrl(path) {
     return `${process.env.REACT_APP_API_URL}${path}`;
   }
   
-
+  // JSX structure for the App component
   return (
     <>
-    <div className="bg-gradient-to-r from-ffbd00 to-[#eca600]">
-    <title > SRPC </title>
-    <div>
-      <Navbar />
-    </div>
-    <div className="container mx-auto px-4 bg-gradient-to-r from-ffbd00 to-[#eca600]">
-    <Refresh />
-    </div>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route exact path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Register/>} />
-        <Route path="/confirmation" element={<Confirmation/>} />
-        <Route exact path="/poster" element={<PrivateRoute permissionCheckUrl={null}><Poster /></PrivateRoute> } />
-        <Route path="/round/1/:posterId" element={<PrivateRoute permissionCheckUrl={getApiUrl("/precheckposter/round1_pre_check/:id")} ><Round round={1}/></PrivateRoute>} />
-        <Route path="/round/2/:posterId" element={<PrivateRoute permissionCheckUrl={getApiUrl("/precheckposter/round2_pre_check/:id")} ><Round round={2}/></PrivateRoute>} />
-        <Route path="/editscore/1/:posterId" element={<PrivateRoute permissionCheckUrl={getApiUrl("/precheckposter/round1_pre_check_edit/:id")}><EditRound round={1} /></PrivateRoute>}/>
-        <Route path="/editscore/2/:posterId" element={<PrivateRoute permissionCheckUrl={getApiUrl("/precheckposter/round2_pre_check_edit/:id")}><EditRound round={2} /></PrivateRoute>}/>
-        {/* display 404 page if no route matches */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js"></script>
+      {/* Background gradient applied to the whole application */}
+      <div className="bg-gradient-to-r from-ffbd00 to-[#eca600]">
+        {/* Setting the title for the application */}
+        <title> SRPC </title>
+        {/* Navbar component, displayed on all pages */}
+        <div>
+          <Navbar />
+        </div>
+        {/* Container for the Refresh component, provides consistent styling */}
+        <div className="container mx-auto px-4 bg-gradient-to-r from-ffbd00 to-[#eca600]">
+          <Refresh />
+        </div>
+        {/* Router setup to handle navigation and routing within the application */}
+        <Router>
+          <Routes>
+            {/* Define the route structure and associate paths with components */}
+            <Route path="/" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/signup" element={<Register/>} />
+            <Route path="/confirmation" element={<Confirmation/>} />
+            {/* Private routes require authentication, redirects if not authenticated */}
+            <Route path="/poster" element={<PrivateRoute><Poster /></PrivateRoute>} />
+            {/* Dynamic routes for different rounds in the application, uses getApiUrl for pre-check */}
+            <Route path="/round/1/:posterId" element={<PrivateRoute permissionCheckUrl={getApiUrl("/precheckposter/round1_pre_check/:id")}><Round round={1}/></PrivateRoute>} />
+            <Route path="/round/2/:posterId" element={<PrivateRoute permissionCheckUrl={getApiUrl("/precheckposter/round2_pre_check/:id")}><Round round={2}/></PrivateRoute>} />
+            <Route path="/editscore/1/:posterId" element={<PrivateRoute permissionCheckUrl={getApiUrl("/precheckposter/round1_pre_check_edit/:id")}><EditRound round={1} /></PrivateRoute>}/>
+            <Route path="/editscore/2/:posterId" element={<PrivateRoute permissionCheckUrl={getApiUrl("/precheckposter/round2_pre_check_edit/:id")}><EditRound round={2} /></PrivateRoute>}/>
+            {/* Fallback route for 404 Not Found page */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </div>
+      {/* React script inclusion for production build */}
+      <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js"></script>
     </>
   );
 }
--- src/Confirmation.js
+++ src/Confirmation.js
@@ -1,43 +1,58 @@
+// Import React and necessary styles
 import React from 'react';
 import './index.css';
+// Import images used in the component
 import successLogo from './images/new_logo_stacked.jpg';
 import doneGif from './images/done.gif';
 
+// Confirmation component used to display the success message after a user action (e.g., registration)
 function Confirmation() {
     return (
         <>
+        {/* Full screen container with a background gradient */}
         <div className="min-h-screen bg-gradient-to-r from-ffbd00 to-[#eca600]">
+            {/* Top margin for spacing */}
             <br/>
+            {/* Centered container for the content */}
             <div className="container mx-auto px-4 ">
-        <div id="successcontainer" className="text-center">
-          <img
-            id="image"
-            src={successLogo}
-            alt="successLogo"
-            className="mx-auto"
-          />
-          <br />
-          <h3 className="text-xl font-semibold my-4">
-            2023 Student Research Poster Competition
-          </h3>
-          <p className="text-xl mb-4">
-            Thank you for registering to be a judge on Saturday, April 22, 2023
-            from 9 am – 2 pm in the UWM Union Ballroom. Watch your email in early
-            April for more details about the event.
-            <br />
-            <b className="font-bold">
-              Questions? Contact ceas-events@uwm.edu
-            </b>
-          </p>
-          <br />
-          <img id="image" src={doneGif} alt="done" className="mx-auto" />
-          <p id="status" className="text-sm mt-2"></p>
+                {/* Text-center aligns all children to the center */}
+                <div id="successcontainer" className="text-center">
+                    {/* Logo image for visual confirmation of success */}
+                    <img
+                        id="image"
+                        src={successLogo}
+                        alt="success logo"
+                        className="mx-auto"
+                    />
+                    {/* Spacing between elements */}
+                    <br />
+                    {/* Event title and date information */}
+                    <h3 className="text-xl font-semibold my-4">
+                        2023 Student Research Poster Competition
+                    </h3>
+                    {/* Confirmation message for the user */}
+                    <p className="text-xl mb-4">
+                        Thank you for registering to be a judge on Saturday, April 22, 2023
+                        from 9 am – 2 pm in the UWM Union Ballroom. Watch your email in early
+                        April for more details about the event.
+                        <br />
+                        {/* Contact information in case of questions */}
+                        <b className="font-bold">
+                            Questions? Contact ceas-events@uwm.edu
+                        </b>
+                    </p>
+                    {/* Additional spacing */}
+                    <br />
+                    {/* Animated GIF for visual feedback */}
+                    <img id="image" src={doneGif} alt="done" className="mx-auto" />
+                    {/* Placeholder for potential status messages */}
+                    <p id="status" className="text-sm mt-2"></p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    </>
+        </>
     );
-  }
-  
+}
 
+// Export the component for use in other parts of the application
 export default Confirmation;
